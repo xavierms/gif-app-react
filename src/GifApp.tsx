f0a1cfc05e59b6e46384481352526e4f4da2e477
xavierms <xavierams08@gmail.com>
2023-06-09 00:04:47 -0400

    Gif app updated

--- src/GifApp.tsx
+++ src/GifApp.tsx
@@ -1,29 +1,37 @@
 import { useState } from "react"
 import { AddCategory } from "./components/AddCategory";
+import { GifGrid } from "./components/GifGrid";
 
 export const GifApp = () => {
-  const obj = ['BDZ', 'DBZ', 'GT'];
+  const obj = ['DBZ'];
   const [ categories, setCategories ] = useState(obj);
 
   const onAddCategory = (newCategory: string) => {
-    setCategories([newCategory,...categories])
+
+    const sameCategory = categories.some( 
+        category => category.toLowerCase() === newCategory.toLowerCase()
+         );
+    if(!sameCategory)
+    setCategories([ newCategory, ...categories ]);
   }
   return (
     <>
       <h1>GifApp</h1>
 
       <AddCategory 
-          onNewCategory={ onAddCategory }
+          onNewCategory={ (value) => onAddCategory(value) }
+          // onNewCategory={ onAddCategory }
         // setCategories={setCategories} 
         />
-      <button type="button" >Agregar</button>
-      <ol>
-        {
-          categories.map(category => {
-            return <li key={ category }>{ category }</li>
-          })
-        }
-      </ol>
+      {/* <button type="button" >Agregar</button> */}
+      {
+          categories.map( ( category )=> (
+            <GifGrid  
+                key={ category }
+                category={ category } />
+
+          ))
+      }
     </>
   )
 }
