1b99dbd1c14c13e6a471150134a3cefbfcfe385f
xavierms <xavierams08@gmail.com>
2023-06-15 23:17:13 -0400

    Footer added

---
+++ src/shared/components/layout/footer/footer.tsx
@@ -0,0 +1,21 @@
+import './footer.css';
+import github from '../../../../assets/github.png';
+import linkedin from '../../../../assets/linkedin.png';
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className='footer-copyright'>
+                <p>
+                    @{new Date().getFullYear()} Xavier Mejia. All right reserved.
+                </p>
+            </div>
+            <div className="socialmedia">
+            <a href="https://github.com/xavierms" target='_blank'><p><img src={github} alt="GitHub" /></p></a>
+            <a href="https://www.linkedin.com/in/xavier-mejia-sanchez-5a6b70b4/" target='_blank' ><p><img src={linkedin} alt="LinkedIn" /></p></a>
+            </div>
+
+        </footer>
+    )
+}
+
+export default Footer;
