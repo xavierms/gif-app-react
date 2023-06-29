87418dbc630bb676f4a395920b4609f67439b257
xavierms <xavierams08@gmail.com>
2023-06-28 21:22:15 -0400

    Global configs added

---
+++ tests/components/GifItem.test.tsx
@@ -0,0 +1,36 @@
+// import { render } from '@testing-library/react';
+
+// import { GifItem } from '../../src/components/GifItem';
+// import React from 'react';
+// import { ImageProps } from '../../src/props/ImageProps';
+// describe('Test in <GifItem />', () => { 
+//     // eslint-disable-next-line @typescript-eslint/no-empty-function
+//     test('should be match with snapshot', () => { 
+
+        
+//         const image: ImageProps ={
+//             type: '',
+//             id: '',
+//             url: '',
+//             slug: '',
+//             bitly_gif_url: '',
+//             bitly_url: '',
+//             embed_url: '',
+//             username: '',
+//             source: '',
+//             title: '',
+//             rating: '',
+//             content_url: '',
+//             source_tld: '',
+//             source_post_url: '',
+//             is_sticker: 0,
+//             import_datetime: '',
+//             trending_datetime: '',
+//             images: undefined,
+//             user: undefined,
+//             analytics_response_payload: '',
+//             analytics: undefined
+//         };
+//         render( <GifItem { ...image }/>)
+//      })
+//  })
